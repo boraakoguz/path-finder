57b162a6f1e3096701918cc60baa2f91045c41e2
Mehmet Emre Şahin <memresahin40@gmail.com>
2023-12-11 18:01:12 +0300

    Feedback and Object classes were added. Some classes may need some changes due to the dependence relations with another classes which were not implemented yet when I implemented these classes.

---
+++ Feedback.java
@@ -0,0 +1,31 @@
+/**
+ * @author Mehmet Emre Şahin
+ * The feedback class for the Path Finder app.
+ */
+public class Feedback {
+
+    //String variable that includes feedback.
+    private String content;
+    //The status of the feedback: 'false' for unhandled feedbacks, 'true' for done ones.
+    private boolean status;
+
+    //Getter and setter for feedback status.
+
+    /**
+     * Getter for feedback. Returns if the feedback is handled.
+     * @return status of feedback.
+     */
+    public boolean getStatus() { return status; }
+
+    /**
+     * Setter for feedback status.
+     * @param status of feedback.
+     */
+    public void setStatus( boolean status ) { this.status = status; }
+
+    /**
+     * Getter for the feedback message.
+     * @param feedback message.
+     */
+    public String getFeedBack() { return content; }
+}
