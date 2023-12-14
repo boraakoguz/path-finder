de3cab6428bbb174d2fd0b65fdbcc7af34b11774
Ertugrul Malkoc <ertugrul.malkoc2@gmail.com>
2023-12-14 16:24:48 +0300

    finished LeftScreenPanel
    Nu button, 1 button, 2 buttons

--- UI/LeftScreenPanel.java
+++ UI/LeftScreenPanel.java
@@ -2,6 +2,10 @@ package UI;
 import javax.swing.JPanel;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Label;
 
 import javax.swing.JButton;
@@ -14,34 +18,90 @@ public class LeftScreenPanel extends JPanel {
     private JLabel labelBuilding = new JLabel("Select Building");
     private JLabel labelFloor = new JLabel("Select Floor");
     private JLabel labelRoom = new JLabel("Select Room");
+    private JComboBox mapBox = new JComboBox<>();
+    private JComboBox buildingBox = new JComboBox<>(); 
+    private JComboBox floorBox = new JComboBox<>();
+    private JComboBox roomBox = new JComboBox<>();
     private JButton button1;
     private JButton button2;
-    private JComboBox mapBox;
-    private JComboBox buildingBox;
-    private JComboBox floorBox;
-    private JComboBox roomBox;
+    GridBagConstraints c = new GridBagConstraints();
+    
 
     public LeftScreenPanel(String title) {
         labelTitle = new JLabel(title);
+        labelTitle.setFont(new Font("Serif", Font.ITALIC, 25));
         setBackground(Color.PINK);
-        add(labelTitle);
-        add(labelMap);
-        add(labelBuilding);
-        add(labelFloor);
-        add(labelRoom);
 
+        setLayout(new GridBagLayout());
+        c.weightx = 1;
+        c.anchor = GridBagConstraints.WEST;        
+        c.fill = GridBagConstraints.BOTH;
+        c.gridwidth = 2;
+        
+        c.insets = new Insets(0, 10, 50, 10);
+        c.gridx = 0;
+        c.gridy = 0;
+        add(labelTitle, c);
 
+        c.insets = new Insets(0, 10, 0, 10);
+        c.gridx = 0;
+        c.gridy = 1;
+        add(labelMap, c);
+
+        c.insets = new Insets(0, 10, 5, 10);
+        c.gridx = 0;
+        c.gridy = 2;
+        add(mapBox, c);
+        
+        c.insets = new Insets(0, 10, 0, 10);
+        c.gridx = 0;
+        c.gridy = 3;
+        add(labelBuilding, c);
+
+        c.insets = new Insets(0, 10, 5, 10);
+        c.gridx = 0;
+        c.gridy = 4;
+        add(buildingBox, c);
+
+        c.insets = new Insets(0, 10, 0, 10);
+        c.gridx = 0;
+        c.gridy = 5;
+        add(labelFloor, c);
+
+        c.insets = new Insets(0, 10, 5, 10);
+        c.gridx = 0;
+        c.gridy = 6;
+        add(floorBox, c);
+
+        c.insets = new Insets(0, 10, 0, 10);
+        c.gridx = 0;
+        c.gridy = 7;
+        add(labelRoom, c);
+
+        c.insets = new Insets(0, 10, 50, 10);
+        c.gridx = 0;
+        c.gridy = 8;
+        add(roomBox, c);
+
+        c.gridwidth = 1;
     }
     
     public LeftScreenPanel(String type, JButton button1) {
         this(type);
         this.button1 = button1;
+        c.gridx = 0;
+        c.gridy = 9;
+        add(button1, c);
+
     }
 
     
     public LeftScreenPanel(String type, JButton button1, JButton button2) {
         this(type, button1);
         this.button2 = button2;
+        c.gridx = 1;
+        c.gridy = 9;
+        add(button2, c);
     }
 
 
