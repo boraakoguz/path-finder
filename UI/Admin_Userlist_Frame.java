b3be5946af315ab3c2d1bfbdafbfd33979ee106f
Ahmet Kenan Ataman <ataman.ahmet.kenan@gmail.com>
2023-12-16 12:16:43 +0300

    userlist added

--- UI/Admin_Userlist_Frame.java
+++ UI/Admin_Userlist_Frame.java
@@ -2,8 +2,10 @@ package UI;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -13,12 +15,15 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import UI.User_Frame.ButAction;
+import Utilities.Feedback;
+import Utilities.User;
 
 public class Admin_Userlist_Frame extends JFrame {
     protected JButton backBut=new JButton();
     protected JPanel contentPane=new JPanel();
     protected Color backGroundpink; //backgroud color
     protected Path_Finder_Frame controller;
+    protected ArrayList<User> userList;
     
     Admin_Userlist_Frame(Path_Finder_Frame c){
         controller=c;
@@ -59,62 +64,16 @@ public class Admin_Userlist_Frame extends JFrame {
         backBut.setBorder(BorderFactory.createEmptyBorder()); //setting borders of buttons 
         contentPane.add(backBut);
 		
-		JLabel lblNewLabel_1 = new JLabel("Editor1_Account");
-		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 25));
-		lblNewLabel_1.setBounds(86, 184, 200, 62);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("Editor");
-		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 25));
-		lblNewLabel_2.setBounds(450, 184, 124, 62);
-		contentPane.add(lblNewLabel_2);
-		
-		JButton btnNewButton = new JButton("REMOVE");
-		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 25));
-		btnNewButton.setBounds(733, 205, 179, 39);
-        btnNewButton.setForeground(Color.WHITE);
-        btnNewButton.setBackground(Color.BLACK);
-        btnNewButton.addActionListener(new ButAction("remove"));
-        btnNewButton.setBorder(BorderFactory.createEmptyBorder());
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("REMOVE");
-		btnNewButton_1.setFont(new Font("Arial", Font.PLAIN, 25));
-		btnNewButton_1.setBounds(733, 292, 179, 39);
-        btnNewButton_1.setForeground(Color.WHITE);
-        btnNewButton_1.setBackground(Color.BLACK);
-        btnNewButton_1.addActionListener(new ButAction("remove"));
-        btnNewButton_1.setBorder(BorderFactory.createEmptyBorder());
-		contentPane.add(btnNewButton_1);
-		
-		JLabel lblNewLabel_2_1 = new JLabel("Admin");
-		lblNewLabel_2_1.setFont(new Font("Arial", Font.PLAIN, 25));
-		lblNewLabel_2_1.setBounds(450, 271, 124, 62);
-		contentPane.add(lblNewLabel_2_1);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("Admin_Account");
-		lblNewLabel_1_1.setFont(new Font("Arial", Font.PLAIN, 25));
-		lblNewLabel_1_1.setBounds(86, 271, 200, 62);
-		contentPane.add(lblNewLabel_1_1);
-		
-		JButton btnNewButton_2 = new JButton("REMOVE");
-		btnNewButton_2.setFont(new Font("Arial", Font.PLAIN, 25));
-		btnNewButton_2.setBounds(733, 372, 179, 39);
-        btnNewButton_2.setForeground(Color.WHITE);
-        btnNewButton_2.setBackground(Color.BLACK);
-        btnNewButton_2.addActionListener(new ButAction("remove"));
-        btnNewButton_2.setBorder(BorderFactory.createEmptyBorder());
-		contentPane.add(btnNewButton_2);
-		
-		JLabel lblNewLabel_2_2 = new JLabel("Editor");
-		lblNewLabel_2_2.setFont(new Font("Arial", Font.PLAIN, 25));
-		lblNewLabel_2_2.setBounds(450, 351, 124, 62);
-		contentPane.add(lblNewLabel_2_2);
-		
-		JLabel lblNewLabel_1_2 = new JLabel("Editor2_Account");
-		lblNewLabel_1_2.setFont(new Font("Arial", Font.PLAIN, 25));
-		lblNewLabel_1_2.setBounds(86, 351, 200, 62);
-		contentPane.add(lblNewLabel_1_2);
+        JPanel userlistPanel=new JPanel();
+		userlistPanel.setBounds( 60, 170, 900,400);
+		Controller cnt = new Controller();
+		cnt.setCurrentMap("Bilkent");
+		userList = cnt.getUserList();
+		userlistPanel.setLayout(new GridLayout(userList.size(),1));
+		for (User user : userList) {
+			UserMenuObject menuObj = new UserMenuObject(user,new ButAction("remove"));
+			userlistPanel.add(menuObj);
+		}
 		
 		JLabel lblNewLabel_3 = new JLabel("Admin Account");
 		lblNewLabel_3.setFont(new Font("Arial", Font.PLAIN, 18));
