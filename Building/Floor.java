99b65754456bd941160032c4506b5ec2b8270681
Sami Bora Akoguz <samiboraakoguz@gmail.com>
2023-12-18 20:21:22 +0300

    Changed Floor

--- Building/Floor.java
+++ Building/Floor.java
@@ -1,16 +1,44 @@
 package Building;
 
 public class Floor extends Space{
-    int floorNumber;
+    protected int floorNumber;
+    protected int downStairX;
+    protected int downStairY;
+    protected int upStairX;
+    protected int upStairY;
     public Floor(String name,int floorNumber) {
         super(name);
         this.floorNumber = floorNumber;
+    }
+    public void setDownStairX(int x){
+        this.downStairX = x;
+    }
+    public void setDownStairY(int y){
+        this.downStairY = y;
+    }
+    public void setUpStairX(int x){
+        this.upStairX = x;
+    }
+    public void setUpStairY(int y){
+        this.upStairY = y;
     }
     public void setFloorNumber(int newNumber){
         this.floorNumber = newNumber;
     }
     public int getFloorNumber(){
         return this.floorNumber;
+    }
+    public int getDownStairX(){
+        return this.downStairX;
+    }
+    public int getDownStairY(){
+        return this.downStairY;
+    }
+    public int getUpStairX(){
+        return this.upStairX;
+    }
+    public int getUpStairY(){
+        return this.upStairY;
     }
     
 }
